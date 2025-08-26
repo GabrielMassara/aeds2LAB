70a410f4e4f8fce51363ce6d5664e2a84968376b
Gabriel Massara <75136519+GabrielMassara@users.noreply.github.com>
2025-08-26 07:22:58 -0300

    fim lab02.c

--- lab02/lab02.c
+++ lab02/lab02.c
@@ -1,35 +1,36 @@
-include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 int main() {
-        char string1[100];
-        char string2[100];
-        char stringFinal[200];
+	char string1[100];
+	char string2[100];
+	char stringFinal[200];
 
-        scanf("%s", string1);
-        scanf("%s", string2);
+	while(scanf("%s", string1)!=EOF) {
+		scanf("%s", string2);			
 
-        int statusString1 = 1;
-        int statusString2 = 1;
+		int statusString1 = 1;
+		int statusString2 = 1;
 
-        int contador = 0;
-        for(int i=0; i<100; i++) {
-                if(string1[i] != '\0' && statusString1) {
-                        stringFinal[contador] = string1[i];
-                        contador++;
-                } else {
-                        statusString1 = 0;
-                }
-                if(string2[i] != '\0' && statusString2) {
-			stringFinal[contador] = string2[i];
-                        contador++;
-                } else {
-                        statusString2 = 0;
-                }
-                if(!statusString1 && !statusString2){
-                        stringFinal[contador] = '\0';
-                        break;
-                }
-        }
-        printf("%s\n", stringFinal);
-        return 0;
+		int contador = 0;
+		for(int i=0; i<100; i++) {
+			if(string1[i] != '\0' && statusString1) {
+				stringFinal[contador] = string1[i];
+				contador++;
+			} else {
+				statusString1 = 0;
+			}
+			if(string2[i] != '\0' && statusString2) {
+				stringFinal[contador] = string2[i];
+				contador++;
+			} else {
+				statusString2 = 0;
+			}
+			if(!statusString1 && !statusString2){
+				stringFinal[contador] = '\0';
+				break;
+			}
+		}
+		printf("%s\n", stringFinal);
+	}
+	return 0;
 }
